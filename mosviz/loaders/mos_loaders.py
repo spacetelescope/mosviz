e9e3c002217d8aea93cf4b8e618aa72d0e6a8113
nmearl <nchlsearl@gmail.com>
2016-10-17 11:23:02 -0400

    Rename component to avoid label collision

--- mosviz/loaders/mos_loaders.py
+++ mosviz/loaders/mos_loaders.py
@@ -18,18 +18,18 @@ def nirpsec_spectrum1d_reader(file_name):
     It then uses the header keywords of the DATA extension
     to detemine the wavelengths.
     """
-    
+
     hdulist = fits.open(file_name)
 
     # make wavelength a seperate component in addition to coordinate
     # so you can plot it on the x axis
-    wavelength = np.linspace(hdulist['DATA'].header['CRVAL1'], 
-        hdulist['DATA'].header['CRVAL1']*hdulist['DATA'].header['CDELT1'], 
+    wavelength = np.linspace(hdulist['DATA'].header['CRVAL1'],
+        hdulist['DATA'].header['CRVAL1']*hdulist['DATA'].header['CDELT1'],
         hdulist['DATA'].header['NAXIS1'])[::-1]
-    
+
     data = Data(label='1D Spectrum')
     data.coords = coordinates_from_header(hdulist[1].header)
-    data.add_component(wavelength, 'Wavelength')
+    data.add_component(wavelength, 'Composed Wavelength')
     data.add_component(hdulist['DATA'].data, 'Spectral Flux')
     data.add_component(hdulist['VAR'].data, 'Variance')
     data.add_component(hdulist['QUALITY'].data, 'Data Quality')
@@ -48,7 +48,7 @@ def nirpsec_spectrum2D_reader(file_name):
     to detemine the wavelengths.
     """
 
-    hdulist = fits.open(file_name)    
+    hdulist = fits.open(file_name)
     data = Data(label='2D Spectrum')
     data.coords = coordinates_from_header(hdulist[1].header)
     data.add_component(hdulist['DATA'].data, 'Spectral Flux')
