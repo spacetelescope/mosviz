75d78fcc9cdeb10e3a032cc97d540a3a2298369c
Daniel D'Avella <ddavella@stsci.edu>
2019-02-22 09:38:09 -0500

    Continue to use astropy_helpers for generating version file

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+import ah_bootstrap
 from setuptools import setup
+from configparser import ConfigParser
+
+# Continue to use astropy_helpers for generating version.py for now
+# Eventually we should be using setuptools_scm
+from astropy_helpers.version_helpers import generate_version_py
+
+conf = ConfigParser()
+conf.read(['setup.cfg'])
+metadata = dict(conf.items('metadata'))
+
+name = metadata['package_name']
+version = metadata['version']
+release = 'dev' in version
+generate_version_py(name, version, release)
+
+
 setup()
